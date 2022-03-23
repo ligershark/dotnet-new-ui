f83657a3ab8fd4c9b76de537b93c4ed26464979b
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2022-03-23 15:12:17 +0000

    Add template

--- Source/DotnetNewUI/Frontend/src/components/Template.vue
+++ Source/DotnetNewUI/Frontend/src/components/Template.vue
@@ -9,19 +9,25 @@
       height="256" />
     <div class="template__title">
       <h2 class="template__title-heading">{{ templateManifest.name }}</h2>
-      <p class="template__type">{{ templateManifest.tags.type }}</p>
+      <p v-if="templateManifest?.tags?.type" class="template__type">
+        {{ templateManifest?.tags?.type }} template
+      </p>
     </div>
-    <p>{{ templateManifest.description }}</p>
     <ui-tags class="template__tags" :tags="tags" />
+    <p class="template__description">{{ templateManifest.description }}</p>
   </div>
 </template>
 
 <script lang="ts">
 import { computed, defineComponent, toRefs } from "vue";
+import Tags from "@/components/Tags.vue";
 import ITemplate from "@/models/ITemplate";
 
 export default defineComponent({
   name: "ui-template",
+  components: {
+    "ui-tags": Tags,
+  },
   props: {
     template: {
       type: Object as () => ITemplate,
@@ -30,17 +36,71 @@ export default defineComponent({
   },
   setup(props) {
     const { template } = toRefs(props);
+
     const iconUrl = computed(() => template.value.base64Icon);
-    const tags = computed(() =>
-      [template.value.templateManifest.tags.language].concat(
-        template.value.templateManifest.classifications
-      )
-    );
+    const tags = computed(() => {
+      const language = template.value?.templateManifest?.tags?.language;
+      if (language) {
+        return [language].concat(
+          template.value.templateManifest.classifications
+        );
+      }
+      return template.value.templateManifest.classifications;
+    });
+
     return {
-      ...template,
+      ...template.value,
       iconUrl,
       tags,
     };
   },
 });
 </script>
+
+<style lang="scss">
+.template {
+  display: grid;
+  column-gap: 10px;
+  row-gap: 4px;
+  grid-template-areas:
+    "icon title"
+    "icon tags"
+    "icon description"
+    "icon .";
+  grid-template-columns: auto 1fr;
+  grid-template-rows: auto auto auto 1fr;
+}
+
+.template__icon {
+  grid-area: icon;
+
+  width: 80px;
+  height: 80px;
+}
+
+.template__title {
+  grid-area: title;
+
+  display: flex;
+  align-items: baseline;
+  gap: 10px;
+  flex-wrap: wrap;
+}
+.template__type {
+  background: #a62dd8;
+  border-radius: 10px;
+  padding: 2px 10px;
+}
+
+.template__tags {
+  grid-area: tags;
+}
+
+.template__description {
+  grid-area: description;
+
+  overflow-wrap: break-word;
+  word-wrap: break-word;
+  word-break: break-word;
+}
+</style>
