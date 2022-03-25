6e5653caf854beb61fa57a29b96275fba6f63c36
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2022-03-25 09:03:12 +0000

    Remove shutdown

--- Source/DotnetNewUI/Frontend/src/main.ts
+++ Source/DotnetNewUI/Frontend/src/main.ts
@@ -4,11 +4,11 @@ import { createApp } from "vue";
 import { createMetaManager } from "vue-meta";
 import App from "./App.vue";
 import router from "./router";
-import { getOriginUrl } from "@/composables/Templates";
+// import { getOriginUrl } from "@/composables/Templates";
 
-window.onbeforeunload = function () {
-  const origin = getOriginUrl();
-  fetch(`${origin}/Shutdown`, { method: "POST", keepalive: true });
-};
+// window.onbeforeunload = function () {
+//   const origin = getOriginUrl();
+//   fetch(`${origin}/Shutdown`, { method: "POST", keepalive: true });
+// };
 
 createApp(App).use(createMetaManager()).use(router).mount("#app");
